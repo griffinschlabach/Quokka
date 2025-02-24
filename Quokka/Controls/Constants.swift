fd774bc97518468aae5048069b71d141cbb30ba4
Griffin Schlabach <griffin.schlabach@gmail.com>
2025-02-24 13:21:45 -0500

    Constant font and colors

---
+++ Quokka/Controls/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Quokka
+//
+//  Created by Griffin Schlabach (student LM) on 2/24/25.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct Constants{
+    static let mainfont: Font = Font(UIFont(name: "Gill Sans", size: 20.0) ?? UIFont.systemFont(ofSize: 5.0))
+    
+}
+
+extension Color {
+    static let c_light_green = Color("c-light-green")
+    static let c_dark_green = Color("c-dark-green")
+    static let c_light_brown = Color("c-light-brown")
+    static let c_dark_brown = Color("c-dark-brown")
+    static let c_blue = Color("c-blue")
+    static let c_orange = Color("c-orange")
+
+
+}
