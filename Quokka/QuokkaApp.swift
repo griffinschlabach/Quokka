4df8727711208faecaa9da3060323bff4ff020b8
Griffin Schlabach <griffin.schlabach@gmail.com>
2025-02-24 12:48:44 -0500

    Initial Commit

---
+++ Quokka/QuokkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuokkaApp.swift
+//  Quokka
+//
+//  Created by Griffin Schlabach (student LM) on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct QuokkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
