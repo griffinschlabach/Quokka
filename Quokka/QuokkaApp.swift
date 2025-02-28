375fd572e11f714d050be7ae691691ec47d4cdb0
Griffin Schlabach <griffin.schlabach@gmail.com>
2025-02-28 13:01:24 -0500

    Firebase + dependencies

--- Quokka/QuokkaApp.swift
+++ Quokka/QuokkaApp.swift
@@ -5,7 +5,20 @@
 //  Created by Griffin Schlabach (student LM) on 2/24/25.
 //
 
+//quokka-bc53d
+
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 
 @main
 struct QuokkaApp: App {
