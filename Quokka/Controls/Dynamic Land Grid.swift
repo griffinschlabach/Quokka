57d98bfef7ddae0da34e754a47441e1c6f89426c
Allison Tan <s024407@students.lmsd.org>
2025-04-01 14:14:38 -0400

    made quokka land struct and view

---
+++ Quokka/Controls/Dynamic Land Grid.swift
@@ -0,0 +1,8 @@
+//
+//  Dynamic Land Grid.swift
+//  Quokka
+//
+//  Created by Allison Tan (student LM) on 4/1/25.
+//
+
+import Foundation
---
+++ Quokka/Views/Land Grid View.swift
@@ -0,0 +1,18 @@
+//
+//  Land Grid View.swift
+//  Quokka
+//
+//  Created by Allison Tan (student LM) on 4/1/25.
+//
+
+import SwiftUI
+
+struct Land_Grid_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Land_Grid_View()
+}
