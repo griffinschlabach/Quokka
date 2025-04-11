e227a090b0be10d22a90aa459461a9dabcb61684
Allison Tan <s024407@students.lmsd.org>
2025-04-11 13:48:44 -0400

    trying animal prod calc and view again

--- Quokka/Controls/Animal Product Calculator.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Animal Product Calculator.swift
-//  Quokka
-//
-//  Created by Allison Tan (student LM) on 4/10/25.
-//
-
-import Foundation
--- Quokka/Controls/Animal Product Land Calculator.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Animal Product Land Calculator.swift
-//  Quokka
-//
-//  Created by Allison Tan (student LM) on 4/11/25.
-//
-
-import Foundation
