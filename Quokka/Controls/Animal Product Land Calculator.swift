17423ed2047a55c5af5f4b6c1bcdc0fbec6d6bc3
Allison Tan <s024407@students.lmsd.org>
2025-04-11 13:33:12 -0400

    animal quiz daily view and calc

---
+++ Quokka/Controls/Animal Product Land Calculator.swift
@@ -0,0 +1,8 @@
+//
+//  Animal Product Land Calculator.swift
+//  Quokka
+//
+//  Created by Allison Tan (student LM) on 4/11/25.
+//
+
+import Foundation
---
+++ Quokka/Views/Animal Product Daily Quiz View.swift
@@ -0,0 +1,18 @@
+//
+//  Animal Product Daily Quiz View.swift
+//  Quokka
+//
+//  Created by Allison Tan (student LM) on 4/11/25.
+//
+
+import SwiftUI
+
+struct Animal_Product_Daily_Quiz_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Animal_Product_Daily_Quiz_View()
+}
